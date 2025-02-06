5b6a08e5cf03db255a70037a847a9f0807029421
asier-acuna <asier.acuna@rai.usc.es>
2025-02-06 12:56:15 +0100

    continuación practica 1

--- representacion_conocimiento/prac1_part1.pl
+++ representacion_conocimiento/prac1_part1.pl
@@ -53,7 +53,7 @@ tiene(homer, cerveza).
 tiene(homer, television).
 capacidad_jarras(2).
 
-% considera_amigo(X,Y) -> X considera amigo a Y
+% considera_amigo(X,Y) ; X considera amigo a Y
 considera_amigo(milhouse, bart).
 
 /* REGLAS */
@@ -64,42 +64,38 @@ considera_amigo(milhouse, bart).
 es_menor(X) :- estudia(X, _).
 es_menor(X) :- bebe(X).
 
-% b. Se é pai de alguén, entón é adulto e é home. Igualmente, se é nai de
-alguén, entón é adulto e é muller.
+% b. Se é pai de alguén, entón é adulto e é home. Igualmente, se é nai de alguén, entón é adulto e é muller.
 adulto(X) :- padre(_, X).
 adulto(X) :- madre(_, X).
 hombre(X) :- padre(_, X).
 mujer(X) :- madre(_, X).
 
-% c. Se dous personaxes distintos son fillos do mesmo pai e da mesma nai,
-entón son irmáns.
+% c. Se dous personaxes distintos son fillos do mesmo pai e da mesma nai, entón son irmáns.
 hermanos(X, Y) :- padre(X, A), padre(Y, A), madre(X, B), madre(Y, B), X \= Y.
 
-% d. Se dous personaxes distintos comparten pai ou nai, pero non ambos,
-entón son medio irmáns.
-medio_hermanos(X, Y) :- padre(X, A), padre(Y, A), madre(X, B1), madre(Y, B2), B1 \= B2, X \= Y.
-medio_hermanos(X, Y) :- madre(X, A), madre(Y, A), padre(X, B1), padre(Y, B2), B1 \= B2, X \= Y.
+% d. Se dous personaxes distintos comparten pai ou nai, pero non ambos, entón son medio irmáns.
+medio_hermanos(X, Y) :- padre(X, A), padre(Y, A), madre(X, B1), madre(Y, B2), B1\=B2, X\=Y.
+medio_hermanos(X, Y) :- madre(X, A), madre(Y, A), padre(X, B1), padre(Y, B2), B1\=B2, X\=Y.
 
 % e. Se é abusón, gústanlle as pintadas.
 gusta(X, pintadas) :- abuson(X).
 
 % f. Se teñen un gusto en común, entón son amigos entre si.
-amigos(X, Y) :- gusta(X, A), gusta(Y, A), X \= Y.
+amigos(X, Y) :- gusta(X, A), gusta(Y, A), X\=Y.
 
 % EJERCICIO 3
 
 % a- Calquera que non sexa vexetariano come no Krusty Burger.
-come(X, krusty_burger) :- \+ es(X, vegetariano).
+come(X, krusty_burger) :- not(es(X, vegetariano)).
 
 % b- Todos os homes maiores de 30 anos e menores de 60 que traballan na central acuden ao bar de Moe.
-come(X, bar_moe) :- hombre(X), edad(X, N), N >= 30, N =< 60, trabaja(X, central_nuclear).
+come(X, bar_moe) :- hombre(X), edad(X, N), N > 30, N < 60, trabaja(X, central_nuclear).
 
-% c- As grande xerras de cervexa do bar de Moe teñen unha capacidade
-de 2 litros. Calcular a cantidade de litros que bebe Homer se bebe N xerras, sendo N un parámetro de entrada, que debe ser sempre un número enteiro maior ou igual a 0.
-litros_bebidos(N, L) :- capacidad_jarras(C), L is N * C.
+% c- As grande xerras de cervexa do bar de Moe teñen unha capacidade de 2 litros. Calcular a cantidade de litros que bebe Homer se bebe N xerras, sendo N un parámetro de entrada, que debe ser sempre un número enteiro maior ou igual a 0 
+litros_bebidos(Num_jarras, L) :- capacidad_jarras(C), L is Num_jarras * Cap.
 
 % d- Sen tele e sen cervexa, Homer perde a cabeza.
-pierde_cabeza(homer) :- \+ tiene(homer, cerveza), \+ tiene(homer, television).
+pierde_cabeza(homer) :- not(tiene(homer, cerveza)), not(tiene(homer, television)).
 
 /* CONSULTAS */
 
