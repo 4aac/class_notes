65fc4dad5d989c5d2520742dd5d90709c6fcf64b
asier-acuna <asier.acuna@rai.usc.es>
2025-02-24 18:40:58 +0100

    final knowledge base

--- representacion_conocimiento/fiesta_homer_base_conocimientos.pl
+++ representacion_conocimiento/fiesta_homer_base_conocimientos.pl
@@ -40,13 +40,13 @@ adulto(luann_van_houten).
 adulto(kirk_van_houten). 
 adulto(clancy_wiggum). 
 adulto(sarah_wiggum).
-niño(bart). 
-niño(lisa). 
-niño(rod_flanders). 
-niño(todd_flanders). 
-niño(milhouse_van_houten). 
-niño(ralph_wiggum).
-bebé(maggie).
+ninho(bart). 
+ninho(lisa). 
+ninho(rod_flanders). 
+ninho(todd_flanders). 
+ninho(milhouse_van_houten). 
+ninho(ralph_wiggum).
+bebe(maggie).
 
 vive(homer, casa_springfield). 
 vive(marge, casa_springfield). 
@@ -86,6 +86,10 @@ trabaja(kirk_van_houten, espantapajaros).
 trabaja(clancy_wiggum, jefe_policia). 
 trabaja(luann_van_houten, oficina_desconocida).
 
+vecino(ned_flanders, simpons).
+vecino(todd_flanders, simpsons).
+vecino(rod_flanders, simpsons).
+
 come(homer, churrasco). 
 come(homer, criollo). 
 come(homer, chorizo). 
@@ -95,6 +99,7 @@ come(marge, chorizo).
 come(bart, churrasco). 
 come(bart, criollo). 
 come(lisa, salchichas_tofu). 
+come(maggie, biberon).
 come(abe, churrasco). 
 come(abe, criollo). 
 come(abe, chorizo). 
@@ -137,17 +142,23 @@ come(ralph_wiggum, salchichas_tofu).
 aficion(homer, beber_bar_moe). 
 aficion(marge, leer).
 aficion(bart, skate). 
-aficion(lisa, jugar_muñecas).
+aficion(lisa, jugar_munhecas).
+aficion(maggie, chupar_chupete).
 aficion(abe, contar_historias). 
+aficion(patty_bouvier, fumar).
+aficion(selma_bouvier, fumar).
 aficion(lenny_leonard, beber_bar_moe). 
 aficion(carl_carlson, beber_bar_moe). 
 aficion(barney_gumble, beber_bar_moe). 
 aficion(moe_szyslak, pescar). 
+aficion(moe_szyslak, hablar_con_amigos).
 aficion(ned_flanders, rezar).
 aficion(rod_flanders, rezar). 
 aficion(todd_flanders, rezar). 
-aficion(edna_krabappel, niños). aficion(luann_van_houten, leer). 
-aficion(kirk_van_houten, comer_palomitas). aficion(milhouse_van_houten, lisa). 
+aficion(edna_krabappel, ninhos). 
+aficion(luann_van_houten, leer). 
+aficion(kirk_van_houten, comer_palomitas). 
+aficion(milhouse_van_houten, lisa). 
 aficion(clancy_wiggum, rosquillas). 
 aficion(sarah_wiggum, leer). 
 aficion(ralph_wiggum, quemar_cosas).
@@ -156,10 +167,27 @@ padre(homer, bart).
 padre(homer, lisa). 
 padre(homer, maggie).  
 padre(abe, homer). 
+padre(ralph_wiggum, clancy_wiggum).
+padre(kirk_van_houten, milhouse_van_houten)
 madre(marge, bart). 
 madre(marge, lisa). 
 madre(marge, maggie).
 madre(jackie_bouvier, marge).
+madre(luann_van_houten, milhouse_van_houten).
+
+hijo(bart, homer).
+hijo(bart, marge).
+hijo(lisa, homer).
+hijo(lisa, marge).
+hijo(maggie, homer).
+hijo(maggie, marge).
+hijo(homer, abe).
+hijo(clancy_wiggum, ralph_wiggum).
+hijo(ralph_wiggum, sarah_wiggum.)
+hijo(marge, jackie_bouvier).
+hijo(milhouse_van_houten, luann_van_houten)
+hijo(milhouse_van_houten, kirk_van_houten)
+
 
 hermanos(patty_bouvier, marge). 
 hermanos(marge, patty_bouvier). 
@@ -167,46 +195,57 @@ hermanos(selma_bouvier, marge).
 hermanos(marge, selma_bouvier). 
 hermanos(patty_bouvier, selma_bouvier). 
 hermanos(selma_bouvier, patty_bouvier).
+hermans(bart, maggie).
+hermanos(maggie, bart).
+hermanos(bart, lisa).
+hermanos(lisa, bart).
+hermanos(lisa, maggie).
+hermanos(maggie, lisa).
+hermanos(rod_flanders, todd_flanders).
+hermanos(todd_flanders, rod_flanders).
+
 
 pareja(homer, marge). 
 pareja(marge, homer). 
 pareja(ned_flanders, edna_krabappel). 
 pareja(edna_krabappel, ned_flanders). 
 pareja(clancy_wiggum, sarah_wiggum).
- pareja(sarah_wiggum, clancy_wiggum).
+pareja(sarah_wiggum, clancy_wiggum).
+
+
+estudia(bart, colegio_primaria).
+estudia(lisa, colegio_primaria).
+estudia(rod_flanders, colegio_religioso).
+milhouse_van_houten(milhouse_van_houten, colegio_primaria).
+estudia(ralph_wiggum, colegio_primaria).
+
 
 
 % REGLAS 
 
 miembro_familia_casa(X) :- vive(X, casa_springfield), X \= maggie.
 
-invitado_por_familia(X) :- miembro_familia_casa(Y), (padre(X,Y); madre(X,Y); hermanos(X,Y)), vive(X, springfield).
+amigo_de_homer(X) :- trabaja(X, central_nuclear), X \= homer, aficion(X, A), aficion(homer, A), vive(X, springfield).
 
-amigo_de_homer(X) :- trabaja(X, central_nuclear), X \= homer, comparte_aficion(X, homer), vive(X, springfield).
+club_lectura(X) :- mujer(X), adulto(X), aficion(X, leer).
 
-comparte_aficion(X, Y) :- aficion(X, A), aficion(Y, A).
+amiga_de_marge(X) :- (vive(X, casa_lado_simpson), adulto(X)); club_lectura(X).
 
-amiga_de_marge(X) :- (vecino_adulto(X, marge); (mujer(X), adulto(X), aficion(X, leer))), vive(X, springfield).
+pareja_invitada(X) :- (amigo_de_homer(Y); amiga_de_marge(Y)), pareja(Y, X), pareja(X, Y), vive(X, springfield).
 
-vecino_adulto(X, _) :- vive(X, casa_lado_simpson), adulto(X).
-
-pareja_invitada(X) :- (amigo_de_homer(Y); amiga_de_marge(Y)), pareja(Y, X), vive(X, springfield).
-
-hijo_invitado(X) :- (amigo_de_homer(Y); amiga_de_marge(Y)), (padre(Y,X); madre(Y,X)), estudia_colegio_primaria(X).
-
-estudia_colegio_primaria(X) :- niño(X), estudia(X, colegio_primaria).
+hijo_invitado(X) :- (amigo_de_homer(Y); amiga_de_marge(Y)), (padre(Y,X); madre(Y,X)), estudia(X, colegio_primaria), ninho(X).
 
 excluido(X) :- trabaja(X, colegio_primaria).
 
-invitado(X) :- (miembro_familia_casa(X); invitado_por_familia(X); amigo_de_homer(X); amiga_de_marge(X); pareja_invitada(X); hijo_invitado(X)), not(excluido(X)).
+invitado(X) :- (miembro_familia_casa(X); invitado_por_familia(X); ((padre(X,Y); madre(X,Y); hermanos(X,Y)); amigo_de_homer(X); amiga_de_marge(X); pareja_invitada(X); hijo_invitado(X))), not(excluido(X)).
 
-comida(X, salchichas_tofu(2)) :- not(come(X, churrasco)), not(come(X, criollo)), not(come(X, chorizo)).
-comida(X, [costilla(1), medio(criollo), medio(chorizo)]) :- come(X, churrasco), come(X, criollo), come(X, chorizo).
-comida(X, [costilla(1), criollo(1)]) :- come(X, churrasco), come(X, criollo), not(come(X, chorizo)).
-comida(X, [costilla(1), chorizo(1)]) :- come(X, churrasco), not(come(X, criollo)), come(X, chorizo).
-comida(X, costilla(2)) :- come(X, churrasco), not(come(X, criollo)), not(come(X, chorizo)).
-comida(X, [criollo(1), chorizo(1)]) :- not(come(X, churrasco)), come(X, criollo), come(X, chorizo).
+comida(X, salchichas_tofu(2)) :- not(come(X, churrasco)), not(come(X, criollo)), not(come(X, chorizo)).  % no comen carne
+comida(X, [costilla(1), medio(criollo), medio(chorizo)]) :- come(X, churrasco), come(X, criollo), come(X, chorizo). % come de todo
+comida(X, [costilla(1), criollo(1)]) :- come(X, churrasco), come(X, criollo), not(come(X, chorizo)).  % come churrasco y criollo
+comida(X, [costilla(1), chorizo(1)]) :- come(X, churrasco), not(come(X, criollo)), come(X, chorizo).  % come churrasco y chorizo
+comida(X, costilla(2)) :- come(X, churrasco), not(come(X, criollo)), not(come(X, chorizo)).  % come churrasco
+comida(X, [criollo(1), chorizo(1)]) :- not(come(X, churrasco)), come(X, criollo), come(X, chorizo).  % come criollo y chorizo
 
-bebida(X, cerveza) :- adulto(X), (aficion(X, beber_bar_moe); aficion(X, ir_bar_moe)).
+bebida(X, cerveza) :- adulto(X), aficion(X, beber_bar_moe).
 bebida(X, vino) :- adulto(X), not(bebida(X, cerveza)).
-bebida(X, limonada) :- niño(X).
+bebida(X, limonada) :- ninho(X).
